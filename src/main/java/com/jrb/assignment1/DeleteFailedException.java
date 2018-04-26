bb58a5968bc80b7130e39c3a20ecdbbcf3d7d19c
Ryan Bodenhamer <ryan@Robs-MacBook.local>
2018-04-25 20:37:04 -0500

    first commit

--- src/main/java/com/jrb/assignment1/DeleteFailedException.java
+++
@@ -1,9 +0,0 @@
-package com.jrb.assignment1;
-
-import org.springframework.dao.DataAccessException;
-
-public class DeleteFailedException extends DataAccessException {
-	public DeleteFailedException(String msg) {
-		super(msg);
-	}
-}
--- src/main/java/com/jrb/assignment1/UpdateFailedException.java
+++
@@ -1,9 +0,0 @@
-package com.jrb.assignment1;
-
-import org.springframework.dao.DataAccessException;
-
-public class UpdateFailedException extends DataAccessException {
-	public UpdateFailedException(String msg) {
-		super(msg);
-	}
-}
